0a21b527d08ff5c4e0634ce33a2fd3d746b4afbc
Mateus <mateushbsb@gmail.com>
2020-04-07 21:05:49 -0300

    model

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-const express = require('express');
+const express    = require('express');
+const mongoose   = require('mongoose');
+const requireDir = require('require-dir');
+
+
+//Iniciando o app
 
 const app = express();
 
+// iniciando o db
+
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser: true, useUnifiedTopology: true});
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+
+// primeira rota
 app.get('/', (req, res) => {
-    res.send('Hellow Word');
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native',
+    });
+    return res.send('Hellow Word');
 });
 
 app.listen(3001);
